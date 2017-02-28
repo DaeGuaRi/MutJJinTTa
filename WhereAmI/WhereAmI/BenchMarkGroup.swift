75b9a688147907597923ce0557cfa8425de9ea3e
임성훈 <CodeFactory@imseonghun-ui-MacBook-Pro.local>
2017-02-28 14:42:55 +0900

    Add a UserLocation

---
+++ WhereAmI/WhereAmI/BenchMarkGroup.swift
@@ -0,0 +1,18 @@
+//
+//  BenchMarkGroup.swift
+//  WhereAmI
+//
+//  Created by 임성훈 on 2017. 2. 28..
+//  Copyright © 2017년 swiftBook. All rights reserved.
+//
+
+import Foundation
+class BenchMarkGroup {
+    var groupName: String
+    var benchMark: Dictionary<String, [String]>
+    
+    init(groupName: String) {
+        self.groupName = groupName
+        benchMark
+    }
+}
